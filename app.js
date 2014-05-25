2c3f2f5ebd8b021ee30ce4b0b8f1bf5c8b57068b
chainhelen <chainhelen@gmail.com>
2014-05-26 00:43:05 +0800

    add->auth

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -14,6 +13,7 @@ var fourzorefour = require('./routes/fourzorefour.js');
 var http = require('http');
 var path = require('path');
 var ejs = require('ejs');
+var SessionStore = require("session-mongoose")(express);
 
 var app = express();
 var server = require('http').createServer(app);
@@ -35,29 +35,56 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+app.use(express.cookieSession({
+	secret: 'chain'
+}));
+app.use(express.session({
+	secret: 'chain',
+	store: new SessionStore({
+		url: "mongodb://root:root@ds051838.mongolab.com:51838/cookie",
+		interval: 120000
+	}),
+	cookie: {
+		maxAge: 90000
+	}
+}));
+
+app.use(function(req, res, next) {
+	res.locals.user = req.session.user;
+	next();
+})
+
 //change public router to pritor the router
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-//app.get('/', routes.page);
-app.get('/index', routes.index);
+app.get('/unity', sina.unity);
+app.get(/\/\S+/, routes.indexAuth);
+
+app.get('/', routes.indexone);
+app.post('/', routes.indexpost);
+
+app.get('/welcome', routes.welcome);
+
+app.get('/index', routes.oldindex);
 app.get('/paint', routes.paint);
 app.get('/website', routes.website);
 app.get('/chat', routes.simple_chat);
 
 app.get('/sina/example1', sina.example1);
-app.get('/unity', sina.unity);
 //app.get('/study/:content', study.script);
-
-app.get('/downloads/:filename',sources.downloads);
+app.get('/downloads/:filename', sources.downloads);
 app.get('/downloads/*', sources.err);
-app.get('/?*',fourzorefour.one);
+app.get('/?*', fourzorefour.one);
 
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
+
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@
  * GET home page.
  */
 
-exports.index = function(req, res) {
-	res.render('index');
+exports.oldindex = function(req, res) {
+	res.render('oldindex');
 };
 
 exports.paint = function(req, res) {
@@ -19,7 +19,59 @@ exports.page = function(req, res) {
 };
 
 exports.simple_chat = function(req, res) {
-	res.render('simple_chat',{
+	res.render('simple_chat', {
 		json: req.ip
 	});
+};
+
+exports.indexone = function(req, res) {
+	if (!req.session.user) {
+		res.render('index', {
+			user: 'ni',
+			title: 'Welcome',
+			think: '猜猜怎么才能进入该网站'
+		});
+	} else {
+		req.session.user = null;
+		res.render('index', {
+			title: 'Logout',
+			think: '哎哟，又来一遍'
+		})
+	}
+};
+
+exports.indexpost = function(req, res) {
+	var User = require('../models/user');
+	var user = {};
+	User.findByName(req.body.username, function(err, obj) {
+		if (err) {
+			console.log("not find");
+		}
+		console.log(obj.password);
+		user["username"] = req.body.username;
+		user["password"] = obj.password;
+
+		if (req.body.username == user.username && req.body.password == user.password) {
+			req.session.user = user;
+			res.redirect('/welcome');
+		}
+
+		res.render('index', {
+			title: 'ERROR',
+			think: '错了,错了,不要乱改密码或者用户啊!'
+		});
+	});
+}
+exports.indexAuth = function(req, res, next) {
+	if (!res.locals.user) {
+		res.render('autherr', {
+			title: 'ERROR',
+			think: '错了，错了，不要乱试'
+		})
+		return;
+	}
+	next();
+}
+exports.welcome = function(req, res){
+  res.render('welcome');
 };
